2476124b5c7ce981267a514d948b6157cad3bfd4
Guy Oron <guyoronweb@gmail.com>
2012-04-04 14:07:56 -0700

    Add a page template to be used in the iframe

---
+++ drupal/sites/all/themes/hms/template.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function hms_preprocess_page(&$vars) {
+  global $user;
+  // Add a template suggestion if the user has the client site role.
+  if (in_array('client site', array_values($user->roles))) {
+    $vars['theme_hook_suggestions'][] = 'page__plain';
+  }
+}
