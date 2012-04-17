c434ac3c03ab90a676d093ce4a0f0be21b7fa38b
Guy Oron <guyoronweb@gmail.com>
2012-04-17 13:47:13 -0700

    Stop using tokenauth

--- drupal/sites/all/themes/hms/template.php
+++ drupal/sites/all/themes/hms/template.php
@@ -6,7 +6,7 @@
 function hms_preprocess_page(&$vars) {
   global $user;
   // Add a template suggestion if the user has the client site role.
-  if (in_array('client site', array_values($user->roles))) {
+  if (in_array('anonymous user', array_values($user->roles))) {
     $vars['theme_hook_suggestions'][] = 'page__plain';
   }
 }
